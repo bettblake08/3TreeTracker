3a9f68896a3425929548c1fff3cf9f452f5d600a
bettblake08 <bettblake08@hotmail.com>
2019-02-02 20:53:26 +0300

    Removed protected endpoints tests

--- app/tests/v1/test_admin.py
+++ app/tests/v1/test_admin.py
@@ -5,8 +5,8 @@ from app.tests.v1.admin.post_new_admin_user_endpoint import \
     TestPostNewAdminUserEndpoint
 from app.tests.v1.admin.post_new_product_endpoint import \
     TestPostNewProductEndpoint
-from app.tests.v1.admin.retrieve_repo_content_by_folder_endpoint import \
-    TestRetrieveRepoContentByFolderEndpoint
+# from app.tests.v1.admin.retrieve_repo_content_by_folder_endpoint import \
+    # TestRetrieveRepoContentByFolderEndpoint
 from app.tests.v1.admin.soft_delete_repo_file_endpoint import \
     TestSoftDeleteRepoFileEndpoint
 from app.tests.v1.admin.update_product_endpoint import \
--- app/tests/v1/test_auth.py
+++ app/tests/v1/test_auth.py
@@ -1,5 +1,5 @@
 """ This module hosts the group of test suites that are related
     to the authentications of user accounts.
 """
-from app.tests.v1.authentication.admin_login_auth_endpoint import TestLoginAuthEndpoint
-from app.tests.v1.authentication.admin_log_out_endpoint import TestLogOutEndpoint
+# from app.tests.v1.authentication.admin_login_auth_endpoint import TestLoginAuthEndpoint
+# from app.tests.v1.authentication.admin_log_out_endpoint import TestLogOutEndpoint
